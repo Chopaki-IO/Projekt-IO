fe446a78cc9489b364a088872a1a7981ef8fa711
Cezary <czarekwenta5@gmail.com>
2020-01-23 21:59:50 +0100

    Opis Historyjki

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+*
+*   Historyjka 1
+*
+*   Jako programista, chcę zobaczyć graf 
+*   pokazujący połączenia pomiędzy plikami 
+*   z kodem źródłowym w moim projekcie
+*
+*
+*   @author     Cezary Wenta <author@example.com>
+*/
+?>
